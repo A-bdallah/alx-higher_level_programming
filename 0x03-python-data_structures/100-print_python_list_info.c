f5d5d53ba66a083225d2a6c514181eb916a3ea39
A-bdallah <abdallah179ahmed@gmail.com>
2024-03-18 19:13:11 -0700

    a

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,27 +1,19 @@
-#include <stdio.h>
 #include <Python.h>
 
 /**
- * print_python_list_info - prints python list info
- *
- * @p: PyObject
- * Return: no return
+ * print_python_list_info - adds a new node at the end of a listint_t list
+ * @p: pointer PyObject
  */
 void print_python_list_info(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
-	PyObject *item;
+	long int l_size;
+	int i;
+	PyListObject *l_obj;
 
-	size = Py_SIZE(p);
-	printf("[*] Size of the Python List = %ld\n", size);
-
-	list = (PyListObject *)p;
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	for (i = 0; i < size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-	}
+	l_size = PyList_Size(p);
+	l_obj = (PyListObject *)p;
+	printf("[*] Size of the Python List = %li\n", l_size);
+	printf("[*] Allocated = %li\n", l_obj->allocated);
+	for (i = 0; i < l_size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(l_obj->ob_item[i])->tp_name);
 }
