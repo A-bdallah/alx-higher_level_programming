c2e39550c07dc8cddc369ef6295680fac3f7a156
A-bdallah <abdallah179ahmed@gmail.com>
2024-03-20 18:15:53 -0700

    a

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string is None:
+    if roman_string is None:
         return 0
-    roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    r_list = []
-    rez = 0
-    for c in roman_string:
-        r_list.append(roman_n.get(c))
-
-    list_len = len(r_list)
-    for i in range(list_len):
-        if i == list_len - 1:
-            rez += r_list[i]
-        elif r_list[i] < r_list[i+1]:
-            rez -= r_list[i]
-        else:
-            rez += r_list[i]
-    return rez
+    if (isinstance(roman_string, str)) is False:
+        return 0
+    roman_nums = {"I": 1,
+                  "V": 5,
+                  "X": 10,
+                  "L": 50,
+                  "C": 100,
+                  "D": 500,
+                  "M": 1000}
+    nextR = ""
+    num = 0
+    for i in range(len(roman_string)):
+        if roman_string[i] in roman_nums.keys():
+            key = roman_string[i]
+            if i < len(roman_string) - 1:
+                nextR = roman_string[i + 1]
+            if i == len(roman_string) - 1:
+                num += roman_nums[key]
+            elif roman_nums[key] < roman_nums[nextR]:
+                num -= roman_nums[key]
+            else:
+                num += roman_nums[key]
+    return num
