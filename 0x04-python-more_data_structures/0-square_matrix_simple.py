c650d398d7fc3d9837d32a75d63656bf3c9f5dcf
A-bdallah <abdallah179ahmed@gmail.com>
2024-03-20 17:40:50 -0700

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda l: list(map(lambda n: n*n, l)), matrix))
+    n_matrix = []
+    for i in matrix:
+        row = []
+        for j in i:
+            row.append(j * j)
+        n_matrix.append(row)
+    return (n_matrix)
