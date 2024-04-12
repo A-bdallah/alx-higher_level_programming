89c302b679fcce02d16e31c1892b3772db6c5287
A-bdallah <abdallah179ahmed@gmail.com>
2024-04-12 16:12:21 -0700

    a

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""say my name module"""
+"""
+
+Function that prints "My name is <first name> <last name>"
+
+"""
 
 
-def say_my_name(first_name="", last_name=""):
-    """print (my_name = first_name + last_name)
-    Arges:
-        first_name: string
-        last_name: string
+def say_my_name(first_name, last_name=""):
     """
-    if (not isinstance(first_name, str)):
+    Function that prints "My name is <first name> <last name>"
+    """
+    if not (isinstance(first_name, str)):
         raise TypeError("first_name must be a string")
-    if (not isinstance(last_name, str)):
+    if not (isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
-    if (first_name == "") and (last_name == ""):
-        raise ValueError("missing two arguments")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    try:
+        print("My name is {:s} {:s}".format(first_name, last_name))
+    except Exception as e:
+        raise Exception(e)
